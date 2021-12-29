7d643411e12fae2e9bbf22e20729231ebba8e075
wnehdals <dongmin960903@gmail.com>
2021-12-29 18:03:12 +0900

    library setting

---
+++ app/src/main/java/com/jdm/menunenaw/MenunenawApp.kt
@@ -0,0 +1,7 @@
+package com.jdm.menunenaw
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MenunenawApp: Application()
---
+++ app/src/main/java/com/jdm/menunenaw/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jdm.menunenaw.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jdm.menunenaw.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
