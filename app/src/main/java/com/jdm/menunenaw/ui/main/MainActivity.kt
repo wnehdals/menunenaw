1db46efe3e55d89d00acfe16427c264ddc5e9639
wnehdals <dongmin960903@gmail.com>
2022-01-05 11:27:02 +0900

    set

--- app/src/main/java/com/jdm/menunenaw/ui/main/MainActivity.kt
+++ app/src/main/java/com/jdm/menunenaw/ui/main/MainActivity.kt
@@ -2,11 +2,19 @@ package com.jdm.menunenaw.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jdm.garam.base.ViewBindingActivity
 import com.jdm.menunenaw.R
+import com.jdm.menunenaw.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : ViewBindingActivity<ActivityMainBinding>() {
+    override val layoutId: Int
+        get() = R.layout.activity_main
+
+    override fun subscribe() {
+        
+    }
+
+    override fun initView() {
+
     }
 }
